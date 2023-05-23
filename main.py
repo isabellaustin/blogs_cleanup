f7a8d1e35e9bea644afd8f846bce48e3ea842379
isabell austin <iaustin@butler.edu>
2023-05-23 13:45:09 -0400

    fleshed out stats

--- main.py
+++ main.py
@@ -44,14 +44,13 @@ if __name__ == "__main__":
 
     """creates a list of inactive users by finding the difference between the list of current users (created id_username list) 
         and all active users"""
-    data = blogs.get_inactive_users(exclude = exclude_users, blogs_users=id_username.values()) #all butler users that
+    inactive_data = blogs.get_inactive_users(exclude = exclude_users, blogs_users=id_username.values())
 
     """creates a list of blogs in the database"""
     user_blogs = {}
     blogs.get_user_blogs(user_blogs, cnx)
     sites = user_blogs.values()
 
-    #============================================================================================================================================
     # STATS
     site_stats = {} #tracks whether a site is kept or archived
     indv_user_stats = {} #tracks users on one site
@@ -81,7 +80,7 @@ if __name__ == "__main__":
             #     keyErr_users.append(u)
             #     print(keyErr_users)
             #     continue
-            if username in data: #if user is inactive
+            if username in inactive_data: #if user is inactive
                 print(f"{Fore.RED}{username} will be removed from {site}")
                 
                 remaining_users-=1
@@ -105,11 +104,13 @@ if __name__ == "__main__":
 
         # print(f"The amount of users removed from {site}: {indv_user_stats[site]['remove']}")
         # print(f"The amount of remaining on {site}: {indv_user_stats[site]['keep']}")
+ 
+ #============================================================================================================================================
 
     # print(users_tbd)
     # print(sites_tbd)
 
-    # DELETE USERS
+    # # DELETE USERS
     # for u in users_tbd:
     #     try:
     #         username = id_username[u]
@@ -119,8 +120,7 @@ if __name__ == "__main__":
     #     blogs.delete_user(u,username) #id, user_login
     #     print(f"{Back.GREEN}{username} was deleted from the database.{Back.RESET}")
 
-
-    # DELETE SITES
+    # # DELETE SITES
     # for u in sites_tbd:
     #     try:
     #         path = sites[u]
@@ -128,19 +128,30 @@ if __name__ == "__main__":
     #         continue
     #     blogs.delete_blog(u,path) #blog_id, path
     #     print(f"{Back.GREEN}{path} was deleted from the database.{Back.RESET}")
-
     
     #============================================================================================================================================
     
     cnx.close()
 
-    print(f"The amount of sites archived: {overall_del_sites}")
-    print(f"The amount of sites kept: {overall_kept_sites}")
+    """output statisitics"""    
+    # BLOGS
+    print(f"Initial amount of sites: {len(sites)}")
 
-    print(f"The amount of Butler users remaining across all sites: {overall_kept_users}")
-    print(f"The amount of Butler users removed across all sites: {overall_del_users}")
+    print(f"Amount of sites kept: {overall_kept_sites}")
+    print(f"Amount of sites archived: {overall_del_sites}")
 
-    print(f"The amount of non-Butler users removed from the site (?): {len(outside_data)}")
+    perc_blog_cleanup = (overall_del_sites / len(sites)) * 100
+    print(f"Percent decrease in blogs: {perc_blog_cleanup}")
 
+    # USERS
+    total_bu_users = overall_kept_users + overall_del_users
+    print(f"Initial amount of Butler users across all sites: {total_bu_users}")
+    print(f"Amount of inactive Butler users across all sites: {len(inactive_data)}")
+    print(f"Amount of non-Butler users across all sites: {len(outside_data)}")
+    
+    print(f"Amount of Butler users remaining across all sites: {overall_kept_users}")
+    print(f"Amount of Butler users removed across all sites: {overall_del_users}")
+    print(f"Amount of non-Butler users removed (?) across all sites: {len(outside_data)}") # are all non butler users being removed?
 
-# what percent of users where inactive/deleted
+    perc_user_cleanup = (overall_del_users / total_bu_users) * 100
+    print(f"Percent decrease in Butler users: {perc_user_cleanup}")
